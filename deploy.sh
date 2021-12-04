bbffad35eea008655213a18b6ccdb0c395983e9c
Jouke Waleson <jouke@waleson.com>
2021-12-04 12:42:45 +0100

    deploy and publish

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
-#wget $(AWS_PROFILE=playground aws lambda get-function --function-name slack-quotes-bot | jq -r .Code.Location) -O src.zip
+#wget $(aws lambda get-function --function-name slack-quotes-bot | jq -r .Code.Location) -O src.zip
 #unzip -f src.zip -d src
 #rm src.zip
 
 cd src
 zip ../src.zip *
 cd ..
-AWS_PROFILE=playground aws lambda update-function-code --function-name slack-quotes-bot --zip-file fileb://src.zip
+aws lambda update-function-code --function-name slack-quotes-bot --zip-file fileb://src.zip --publish | cat
 rm src.zip
